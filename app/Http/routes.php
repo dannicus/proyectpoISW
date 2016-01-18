8fd9a458f45caad2ea42fc5fed3b7b8f589d6f50
Daniel Peña <daniel.pena1401@alumnos.ubiobio.cl>
2016-01-18 02:26:26 -0300

    Modificacion de las Rutas del proyecto anterior

---
+++ app/Http/routes.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::controllers([
+	'/' => 'ControladorLogin',
+	'profesor' => 'ProfesorController',
+	'alumno' => 'AlumnoController',
+	'auth' => 'Auth\AuthController',
+	'password' => 'Auth\PasswordController',
+]);
+
+Route::resource('alumno', 'AlumnoController');
+
+// Nos mostrará el formulario de login.
+Route::get('/', 'ControladorLogin@getIndex');
+
+// Validamos los datos de inicio de sesión.
+Route::post('/', 'ControladorLogin@postLogin');
+
+// Nos indica que las rutas que están dentro de él sólo serán mostradas si antes el usuario se ha autenticado.
+Route::group(array('before' => 'auth'), function()
+{
+	// Esta será nuestra ruta de bienvenida.
+	Route::get('/', 'ControladorLogin@getIndex');
+
+	// Esta ruta nos servirá para cerrar sesión.
+	Route::get('logout', 'ControladorLogin@logOut');
+});
