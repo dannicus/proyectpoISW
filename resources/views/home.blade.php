f7dd4eed1ae54304e2b23d44153c4d5d5622ee98
Daniel Peña <daniel.pena1401@alumnos.ubiobio.cl>
2016-01-19 10:25:51 -0300

    home

---
+++ resources/views/home.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.masterLogin')
+
+@section('titulo')
+	SAGGAP
+@stop
+
+@section('css')
+	<link href="{{ asset('css/login_estilo.css') }}" rel="stylesheet">
+@stop
+
+
+@section('content')
+<div class="container">
+	<div class="row">
+		<div class="col-md-10 col-md-offset-1">
+			<div class="panel panel-default">
+				<div class="panel-heading">Home</div>
+
+				<div class="panel-body">
+					MENU PRINCIPAL
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+@endsection
