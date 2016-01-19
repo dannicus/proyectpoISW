46ff3f8c33a489a099492265b879ba28e9411bf3
Daniel Peña <daniel.pena1401@alumnos.ubiobio.cl>
2016-01-19 17:38:05 -0300

    prueba

--- resources/views/layouts/masterLogin.blade.php
+++ resources/views/layouts/masterLogin.blade.php
@@ -39,12 +39,12 @@
 
                 <ul class="nav navbar-nav navbar-right">
                     @if (Auth::guest())
-                        <li><a href="{!! URL::to('/auth/register') !!}">Registrar</a></li>
+                        <li><a href="{!! URL::to('auth/register') !!}">Registrar</a></li>
                     @else
                         <li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{!!  Auth::user()->nombre !!} <span class="caret"></span></a>
                             <ul class="dropdown-menu" role="menu">
-                                <li><a href="{!! URL::to('/auth/logout') !!}">Cerrar Sesion</a></li>
+                                <li><a href="{!! URL::to('auth/logout') !!}">Cerrar Sesion</a></li>
                             </ul>
                         </li>
                     @endif
